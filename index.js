50cd0ad5b1c7f9ea4b82d3814ea2ae12a59dd180
ulkiobal <ulkiobal@users.noreply.github.com>
2021-05-14 15:56:12 +0200

    Rutas definidas

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const superherois = require('./routes/superherois');
 
 mongoose.connect('mongodb://localhost/superherois', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+app.use('/superheroes', superherois);
+
+app.all('*', (req, res) => {
+    res.send('Recurso no encontrado')
+});
 
 app.listen(3000);
---
+++ routes/superherois.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Superheroi = require('../models/superherois');
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.get('/search/:text', (req, res) => {
+
+});
+
+router.get('/sort/:prop', (req, res) => {
+
+});
+
+router.get('/vs/:idFirst/:idSecond', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
